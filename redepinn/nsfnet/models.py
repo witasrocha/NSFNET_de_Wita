d3657eff369c8a9259f9985e8dfac3c385e22cac
PRH01 <witasrocha@gmail.com>
2023-05-25 14:49:15 -0300

    model adicionado

--- redepinn/nsfnet/models.py
+++ redepinn/nsfnet/models.py
@@ -4,9 +4,12 @@ import numpy as np
 
 
 # Load Data
-train_ini1 = np.load('../../datasets/train_ini2.npy')
-train_iniv1 = np.load('../../datasets/train_iniv2.npy')
-train_inip1 = np.load('../../datasets/train_inip2.npy')
-train_xb1 = np.load('../../datasets/train_xb2.npy')
-train_vb1 = np.load('../../datasets/train_vb2.npy')
-train_pb1 = np.load('../../datasets/train_pb2.npy')
+train_ini1 = np.load('C:/Users/PRH01/PycharmProjects/test/NSFNET_de_Wita/datasets/train_iniv1.npy')
+train_iniv1 = np.load('C:/Users/PRH01/PycharmProjects/test/NSFNET_de_Wita/datasets/train_iniv1.npy')
+train_inip1 = np.load('C:/Users/PRH01/PycharmProjects/test/NSFNET_de_Wita/datasets/train_iniv1.npy')
+train_xb1 = np.load('C:/Users/PRH01/PycharmProjects/test/NSFNET_de_Wita/datasets/train_iniv1.npy')
+train_vb1 = np.load('C:/Users/PRH01/PycharmProjects/test/NSFNET_de_Wita/datasets/train_iniv1.npy')
+train_pb1 = np.load('C:/Users/PRH01/PycharmProjects/test/NSFNET_de_Wita/datasets/train_iniv1.npy')
+
+
+
