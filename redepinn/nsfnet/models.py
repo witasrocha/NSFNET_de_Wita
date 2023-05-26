8306f8f747f9124580812a255d52678e82377187
Witã Santos <witasrocha@gmail.com>
2023-05-25 22:32:30 -0300

    ajustes

--- redepinn/nsfnet/models.py
+++ redepinn/nsfnet/models.py
@@ -3,15 +3,15 @@ import numpy as np
 # Create your models here.
 
 # RESOLVER ESSA QUESTÃO DO CARREGAMENTO DAS BASES DE DADOS
-class Dataset(models.Model):
+
 # Load Data
-    def load_train_1(self):
-        return np.load('C:/Users/PRH01/PycharmProjects/test/NSFNET_de_Wita/datasets/train_iniv1.npy')
-        train_iniv1 = np.load('C:/Users/PRH01/PycharmProjects/test/NSFNET_de_Wita/datasets/train_iniv1.npy')
-        train_inip1 = np.load('C:/Users/PRH01/PycharmProjects/test/NSFNET_de_Wita/datasets/train_iniv1.npy')
-        train_xb1 = np.load('C:/Users/PRH01/PycharmProjects/test/NSFNET_de_Wita/datasets/train_iniv1.npy')
-        train_vb1 = np.load('C:/Users/PRH01/PycharmProjects/test/NSFNET_de_Wita/datasets/train_iniv1.npy')
-        train_pb1 = np.load('C:/Users/PRH01/PycharmProjects/test/NSFNET_de_Wita/datasets/train_iniv1.npy')
+
+# train_inil = np.load('C:/Users/PRH01/PycharmProjects/test/NSFNET_de_Wita/datasets/train_iniv1.npy')
+# train_iniv1 = np.load('C:/Users/PRH01/PycharmProjects/test/NSFNET_de_Wita/datasets/train_iniv1.npy')
+# train_inip1 = np.load('C:/Users/PRH01/PycharmProjects/test/NSFNET_de_Wita/datasets/train_iniv1.npy')
+# train_xb1 = np.load('C:/Users/PRH01/PycharmProjects/test/NSFNET_de_Wita/datasets/train_iniv1.npy')
+# train_vb1 = np.load('C:/Users/PRH01/PycharmProjects/test/NSFNET_de_Wita/datasets/train_iniv1.npy')
+# train_pb1 = np.load('C:/Users/PRH01/PycharmProjects/test/NSFNET_de_Wita/datasets/train_iniv1.npy')
 
 
 
--- redepinn/nsfnet/views.py
+++ redepinn/nsfnet/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.db import models
+import numpy as np
 # Create your views here.
+
+#carregando as bases de dados
+
+train_inil = np.load('C:/Users/User/NSFNET_de_Wita/datasets/train_iniv1.npy')
+train_iniv1 = np.load('C:/Users/User/NSFNET_de_Wita/datasets/train_iniv1.npy')
+train_inip1 = np.load('C:/Users/User/NSFNET_de_Wita/datasets/train_iniv1.npy')
+train_xb1 = np.load('C:/Users/User/NSFNET_de_Wita/datasets/train_iniv1.npy')
+train_vb1 = np.load('C:/Users/User/NSFNET_de_Wita/datasets/train_iniv1.npy')
+train_pb1 = np.load('C:/Users/User/NSFNET_de_Wita/datasets/train_iniv1.npy')
+
 
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -11,4 +21,5 @@ def index(request):
     return HttpResponse("Página inicial NSFNET")
 
 def train_1(request):
-    return HttpResponse(models.Dataset(models.Model))
+    # v1 = apps.get_model(model_name=models)
+    return HttpResponse(train_inil)
